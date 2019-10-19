23fa21dd4182cd31d7208245e93141f2e627fb84
Cytosine <pengnan.fan@mail.mcgill.ca>
2019-10-19 17:17:13 -0400

    123

---
+++ q1_1.s
@@ -0,0 +1,17 @@
+			.text
+			.global _start
+
+_start:
+			MOV		R0,		#2
+			PUSH	{R0}
+			MOV		R0,		#3
+			PUSH	{R0}
+			MOV		R0,		#4
+			PUSH	{R0}
+			MOV 	R1,		#0
+			MOV 	R1,		#0
+			MOV 	R1,		#0
+			POP		{R1, R2, R3}			
+
+END:
+			B END
