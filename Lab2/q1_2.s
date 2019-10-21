b21df51730603cc8b089f78fee9f1aa77402ed5f
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2019-10-21 18:57:01 -0400

    Update q1_2.s

--- Lab2/q1_2.s
+++ Lab2/q1_2.s
@@ -2,48 +2,39 @@
 			.global _start
 
 _start:		
-			MOV R0, #89
+			MOV R0, #89		//to assign three numbers to 3 independent registers
 			MOV R1, #71 
 			MOV R2, #84 
 
-			MOV R3, #91 
-
+			MOV R3, #91 		//to build a stack on register,R3,with built in function "PUSH"
 			PUSH {R3}
 			MOV R3, #27
 			PUSH {R3}
 			
 			
 
-			BL FIND_MIN
+			BL FIND_MIN		//do comparision 
 
-			POP {R1}
+			POP {R1}		//extract numbers from the stack 
 			POP {R2}
 			BL FIND_MIN 
 
 			
-			
-			STR R0, [R0]
+			STR R0, [R0]		//store min_value in the according memory address 
 
-END:		
-			B END
+END:			B END
 			 
 
-FIND_MIN:	
-			CMP R0, R1
+FIND_MIN:		CMP R0, R1
 			BGE X 
 
-A:			
-			CMP R0, R2
+A:			CMP R0, R2
 			BGE Y
 			B Z
 
-X:			
-			MOV R0, R1
+X:			MOV R0, R1
 			B A
 
-Y:			
-			MOV R0, R2
+Y:			MOV R0, R2
 
-Z:			
-			BX LR
-
+Z:			BX LR
