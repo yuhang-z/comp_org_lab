f3c94efdf1619f899c86507725e8c5ef0e215419
YuhangZhang <yuhang.zhang@mail.mcgill.ca>
2019-11-04 16:05:20 -0500

    Update HEX_displays.s

--- Lab3/drivers/asm/HEX_displays.s
+++ Lab3/drivers/asm/HEX_displays.s
@@ -32,36 +32,36 @@ LOAD_MULTIPLE_VAR:
         PUSH {R2, R3, R4, R5, R6, R7}
         LDR R2, =HEX_A
         LDR R3, =HEX_B
-		MOV R7, #0xFF
+	MOV R7, #0xFF
         MOV R4, #-1
-		MOV R5, #1
+	MOV R5, #1
 AST:    ADD R4, R4, #1
         CMP R4, #6
         BGE FIN
         TST R0, R5, LSL R4
         BEQ AST
-		LDR R6, [R2]
-		CMP R4, #0
+	LDR R6, [R2]
+	CMP R4, #0
         BICEQ R6, R6, R7
         ORREQ R6, R6, R1
-		CMP R4, #1
+	CMP R4, #1
         BICEQ R6, R6, R7, ROR #24
         ORREQ R6, R6, R1, ROR #24
-		CMP R4, #2
+	CMP R4, #2
         BICEQ R6, R6, R7, ROR #16
         ORREQ R6, R6, R1, ROR #16
         CMP R4, #3
         BICEQ R6, R6, R7, ROR #8
         ORREQ R6, R6, R1, ROR #8
-		STR R6, [R2]
-		LDR R6, [R3]
+	STR R6, [R2]
+	LDR R6, [R3]
         CMP R4, #4
         BICEQ R6, R6, R7
         ORREQ R6, R6, R1
         CMP R4, #5
         BICEQ R6, R6, R7, ROR #24
         ORREQ R6, R6, R1, ROR #24
-		STR R6, [R3]
+	STR R6, [R3]
         B AST
 FIN:    POP {R2, R3, R4, R5, R6, R7}
         BX LR
