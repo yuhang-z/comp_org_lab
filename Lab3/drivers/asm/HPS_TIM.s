3b0a65c656061ce96e9501d5538325bfe68f9a0a
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2019-11-06 17:38:16 -0500

    add comments to part2

--- Lab3/drivers/asm/HPS_TIM.s
+++ Lab3/drivers/asm/HPS_TIM.s
@@ -1,8 +1,4 @@
 	.text
-	.equ   HPS_TIM0_BASE, 0xFFC08000
-	.equ   HPS_TIM1_BASE, 0xFFC09000
-	.equ   HPS_TIM2_BASE, 0xFFD00000
-	.equ   HPS_TIM3_BASE, 0xFFD01000
 	
 	.global HPS_TIM_config_ASM
 	.global HPS_TIM_clear_INT_ASM
@@ -10,13 +6,13 @@
 
 HPS_TIM_config_ASM:
 	PUSH {R4-R7, LR}
-	MOV R1, #0
-	MOV R2, #1
-	LDR R7, [R0]
+	MOV R1, #0		//HPS_TIM_t offset
+	MOV R2, #1		//Used to determine HPS_TIM_t
+	LDR R7, [R0]	//Contents in Construct
 	B LOOP
 
 LOOP:
-	TST R7, R2, LSL R1
+	TST R7, R2, LSL R1		//Chcek if find the corresponding HPS_TIM_t
 	BEQ CONTINUE
 	BL CONFIG
 
@@ -33,8 +29,8 @@ DONE:
 CONFIG:
 	PUSH {LR}
 	
-	LDR R3, =HPS_TIM_BASE
-	LDR R4, [R3, R1, LSL #2]
+	LDR R3, =HPS_TIM_BASE		//Pointer of lists of timer
+	LDR R4, [R3, R1, LSL #2]	//Find corresponding HPS Timer
 	
 	BL DISABLE
 	BL SET_LOAD_VAL
@@ -46,43 +42,43 @@ CONFIG:
 	BX LR 
 
 DISABLE:
-	LDR R5, [R4, #0x8]
-	AND R5, R5, #0xFFFFFFFE
-	STR R5, [R4, #0x8]
+	LDR R5, [R4, #0x8]		//load control memory to R5
+	AND R5, R5, #0xFFFFFFFE	//Set E to 0
+	STR R5, [R4, #0x8]		//Write back to control memory
 	BX LR
 	
 SET_LOAD_VAL:
-	LDR R5, [R0, #0x4]
-	MOV R6, #25
-	MUL R5, R5, R6
-	CMP R1, #2
+	LDR R5, [R0, #0x4]		//load timeout to R5
+	MOV R6, #25				//load 11001 to R6
+	MUL R5, R5, R6			//R5 x R6
+	CMP R1, #2				//change load value if use 50MHz timer
 	LSLLT R5, R5, #2
-	STR R5, [R4]
+	STR R5, [R4]			//load to load value memory
 	BX LR
 	
 SET_LOAD_BIT:
-	LDR R5, [R4, #0x8]
-	LDR R6, [R0, #0x8]
-	AND R5, R5, #0xFFFFFFFD
-	ORR R5, R5, R6, LSL #1
-	STR R5, [R4, #0x8]
+	LDR R5, [R4, #0x8]		//load control memory to R5
+	LDR R6, [R0, #0x8]		//load LD_en to R6
+	AND R5, R5, #0xFFFFFFFD	//set M to 0
+	ORR R5, R5, R6, LSL #1	//write LD_en to M
+	STR R5, [R4, #0x8]		//write control memory back
 	BX LR
 	
 SET_INT_BIT:
-	LDR R5, [R4, #0x8]
-	LDR R6, [R0, #0xC]
-	EOR R6, R6, #0x00000001
-	AND R5, R5, #0xFFFFFFFB
-	ORR R5, R5, R6, LSL #2
+	LDR R5, [R4, #0x8]		//load control memory to R5
+	LDR R6, [R0, #0xC]		//load INT_en to R6
+	EOR R6, R6, #0x00000001	
+	AND R5, R5, #0xFFFFFFFB	//Set I to 0
+	ORR R5, R5, R6, LSL #2	//write INT_en to I
 	STR R5, [R4, #0x8]
 	BX LR
 	
 SET_EN_BIT:
-	LDR R5, [R4, #0x8]
-	LDR R6, [R0, #0x10]
-	AND R5, R5, #0xFFFFFFFE
-	ORR R5, R5, R6
-	STR R5, [R4, #0x8]
+	LDR R5, [R4, #0x8]		//load control memory to R5
+	LDR R6, [R0, #0x10]		//load enable to R6
+	AND R5, R5, #0xFFFFFFFE	//Set E to 0
+	ORR R5, R5, R6			//Write enable to E
+	STR R5, [R4, #0x8]		//Write control memory back
 	BX LR
 
 HPS_TIM_clear_INT_ASM:
--- Lab3/main.c
+++ Lab3/main.c
@@ -8,38 +8,38 @@
 #include "./drivers/inc/int_setup.h"
 
 int part1(){
-
 	while (1) {
-
         int readInteger = read_slider_switches_ASM();
-
 		write_LEDs_ASM(readInteger);		
-
         int toHEXDisplays = read_PB_data_ASM() & 0x0000000F;
-
         char readChar = (char)(readInteger & 0x0000000F);
-
         int isClear = readInteger & 0x00000200;
-
         if (isClear) {
-
             HEX_clear_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5);
-
         } else {
-
             HEX_flood_ASM(HEX4 | HEX5);
-			//HEX_flood_ASM(HEX4);
-			//HEX_flood_ASM(HEX5);
-
             HEX_write_ASM(toHEXDisplays, readChar);
-
     	}
 	}
 	return 0;
 }
 
-int part2(HPS_TIM_config_t hps_tim10ms, HPS_TIM_config_t hps_tim5ms){
+int part2(){
 	unsigned int count = 0, start = 0;
+
+	HPS_TIM_config_t hps_tim10ms;
+	hps_tim10ms.tim = TIM0;
+	hps_tim10ms.timeout = 10000;
+	hps_tim10ms.LD_en = 1;
+	hps_tim10ms.INT_en = 1;
+	hps_tim10ms.enable = 1;
+
+    HPS_TIM_config_t hps_tim5ms;
+	hps_tim5ms.tim = TIM1;
+	hps_tim5ms.timeout = 5000;
+	hps_tim5ms.LD_en = 1;
+	hps_tim5ms.INT_en = 1;
+	hps_tim5ms.enable = 1;	
 
 	HPS_TIM_config_ASM(&hps_tim10ms);
 	HPS_TIM_config_ASM(&hps_tim5ms);
@@ -105,95 +105,9 @@ int	main()	{
 	/*	Part 1	*/
 	//return part1();
 	
-	HPS_TIM_config_t hps_tim10ms;
-	hps_tim10ms.tim = TIM0;
-	hps_tim10ms.timeout = 10000;
-	hps_tim10ms.LD_en = 1;
-	hps_tim10ms.INT_en = 1;
-	hps_tim10ms.enable = 1;
-
-    HPS_TIM_config_t hps_tim5ms;
-	hps_tim5ms.tim = TIM1;
-	hps_tim5ms.timeout = 5000;
-	hps_tim5ms.LD_en = 1;
-	hps_tim5ms.INT_en = 1;
-	hps_tim5ms.enable = 1;
-	return part2(hps_tim10ms, hps_tim5ms);
-	/*
-	unsigned int count = 0, start = 0;
-
-	HPS_TIM_config_t hps_tim10ms;
-	hps_tim10ms.tim = TIM0;
-	hps_tim10ms.timeout = 10000;
-	hps_tim10ms.LD_en = 1;
-	hps_tim10ms.INT_en = 1;
-	hps_tim10ms.enable = 1;
-
-    HPS_TIM_config_t hps_tim5ms;
-	hps_tim5ms.tim = TIM1;
-	hps_tim5ms.timeout = 5000;
-	hps_tim5ms.LD_en = 1;
-	hps_tim5ms.INT_en = 1;
-	hps_tim5ms.enable = 1;
-
-	HPS_TIM_config_ASM(&hps_tim10ms);
-	HPS_TIM_config_ASM(&hps_tim5ms);
-
-	while (1) {
-
-		if (HPS_TIM_read_INT_ASM(TIM1)) {
-
-			HPS_TIM_clear_INT_ASM(TIM1);
-
-			if (read_PB_edgecap_ASM()) {
-
-				if (PB_edgecap_is_pressed_ASM(PB0)) start = 1;
-
-				if (PB_edgecap_is_pressed_ASM(PB1)) start = 0;
-
-				if (PB_edgecap_is_pressed_ASM(PB2)) {
-
-					start = 0;
-
-					count = 0;
-
-					HEX_write_ASM(HEX0, 0);
-					HEX_write_ASM(HEX1, 0);
-					HEX_write_ASM(HEX2, 0);
-					HEX_write_ASM(HEX3, 0);
-					HEX_write_ASM(HEX4, 0);
-					HEX_write_ASM(HEX5, 0);
-
-				}
-
-				PB_clear_edgecp_ASM(HEX0);
-				PB_clear_edgecp_ASM(HEX1);
-				PB_clear_edgecp_ASM(HEX2);
-				PB_clear_edgecp_ASM(HEX3);
-				PB_clear_edgecp_ASM(HEX4);
-				PB_clear_edgecp_ASM(HEX5);
-
-			}
-
-		}
-
-		if (start && HPS_TIM_read_INT_ASM(TIM0)) {
-
-			HPS_TIM_clear_INT_ASM(TIM0);
-
-			count = (count + 1) % 600000;
-
-			HEX_write_ASM(HEX0, count % 10);
-			HEX_write_ASM(HEX1, (count / 10) % 10);
-			HEX_write_ASM(HEX2, (count / 100) % 10);
-			HEX_write_ASM(HEX3, ((count / 100) % 60) / 10);
-			HEX_write_ASM(HEX4, ((count / 100) / 60) % 10);
-			HEX_write_ASM(HEX5, ((count / 100) / 60) / 10 % 10);
-
-		}
-
-	}
-	*/
+	/* Part 2 */
+	return part2();
+	
 	
 	/*
 	int_setup(2, (int []){73, 199});
