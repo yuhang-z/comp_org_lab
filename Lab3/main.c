f035f18b008845ade0919b0ec498e852de107e5a
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2019-11-08 10:35:06 -0500

    edit based on source

--- Lab3/main.c
+++ Lab3/main.c
@@ -32,6 +32,52 @@ int part1(){
 }
 
 int part2(){
+	unsigned int ms = 0, start = 0, s = 0, min = 0;
+	HPS_TIM_config_t timer;
+	timer.tim = TIM0;
+	timer.timeout = 10000;
+	timer.LD_en = 1;
+	timer.INT_en = 1;
+	timer.enable = 1;
+	
+	HPS_TIM_config_ASM(&timer);
+	HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
+	while(1){
+		if (read_PB_edgecap_ASM()){
+			if (PB_edgecap_is_pressed_ASM(PB0)){start = 1;}
+
+			if (PB_edgecap_is_pressed_ASM(PB1)){start = 0;}
+			
+			if (PB_edgecap_is_pressed_ASM(PB2)) {
+				start = 0;
+				ms = 0;
+				s = 0;
+				min = 0;
+				HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
+			}
+			
+			PB_clear_edgecp_ASM(PB0);
+			PB_clear_edgecp_ASM(PB1);
+			PB_clear_edgecp_ASM(PB2);
+		}
+		if (start&&HPS_TIM_read_INT_ASM(TIM0)){
+			HPS_TIM_clear_INT_ASM(TIM0);
+			ms++;
+			s += (ms%100==0&&ms!=0) ? 1 : 0;
+			ms %= 100;
+			min += (s%60==0&&s!=0) ? 1 : 0;
+			s %= 60;
+			min %= 100;
+			HEX_write_ASM(HEX0, ms%10);
+			HEX_write_ASM(HEX1, (ms/10)%10);
+			HEX_write_ASM(HEX2, s%10);
+			HEX_write_ASM(HEX3, (s/10)%10);
+			HEX_write_ASM(HEX4, min%10);
+			HEX_write_ASM(HEX5, (min/10)%10);
+		}
+	}
+	return 0;
+	/*
 	int count0 = 0, count1 = 0, count2 = 0, count3 = 0;
 
 	int start = 0;	
@@ -124,26 +170,22 @@ int part2(){
 		}
 	}
 	return 0;
+	*/
 }
 
 int part3(){
 	int_setup(1, (int[]){199});
-	
-	int count = 0;
-	
-	int start = 0;
+	unsigned int ms = 0, start = 0, s = 0, min = 0;
 
-	HPS_TIM_config_t hps_tim;
-	hps_tim.tim = TIM0;
-	hps_tim.timeout = 1000000;
-	hps_tim.LD_en = 1;
-	hps_tim.INT_en = 1;
-	hps_tim.enable = 1;
-
-	HPS_TIM_config_ASM(&hps_tim);
+	HPS_TIM_config_t timer;
+	timer.tim = TIM0;
+	timer.timeout = 10000;
+	timer.LD_en = 1;
+	timer.INT_en = 1;
+	timer.enable = 1;
 	
-	HEX_write_ASM(HEX0, count);
-
+	HPS_TIM_config_ASM(&timer);
+	HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
 	while(1){
 		if (read_PB_edgecap_ASM()){
 			if (PB_edgecap_is_pressed_ASM(PB0)){start = 1;}
@@ -152,78 +194,33 @@ int part3(){
 			
 			if (PB_edgecap_is_pressed_ASM(PB2)) {
 				start = 0;
-				count = 0;
-				HEX_write_ASM(HEX0, count);
+				ms = 0;
+				s = 0;
+				min = 0;
+				HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
 			}
+			
 			PB_clear_edgecp_ASM(PB0);
 			PB_clear_edgecp_ASM(PB1);
 			PB_clear_edgecp_ASM(PB2);
 		}
-		if (start && hps_tim0_int_flag){
+		if (start&&hps_tim0_int_flag){
 			hps_tim0_int_flag = 0;
-			count++;
-			count%=16;
-			HEX_write_ASM(HEX0, count);
+			ms++;
+			s += (ms%100==0&&ms!=0) ? 1 : 0;
+			ms %= 100;
+			min += (s%60==0&&s!=0) ? 1 : 0;
+			s %= 60;
+			min %= 100;
+			HEX_write_ASM(HEX0, ms%10);
+			HEX_write_ASM(HEX1, (ms/10)%10);
+			HEX_write_ASM(HEX2, s%10);
+			HEX_write_ASM(HEX3, (s/10)%10);
+			HEX_write_ASM(HEX4, min%10);
+			HEX_write_ASM(HEX5, (min/10)%10);
 		}
-	}
-	/*
-	int_setup(2, (int []){73, 199});
+	}	
 
-	enable_PB_INT_ASM(PB0 | PB1 | PB2);
-
-    unsigned int count = 0, start = 0;
-
-	HPS_TIM_config_t hps_tim10ms;
-	hps_tim10ms.tim = TIM0;
-	hps_tim10ms.timeout = 10000;
-	hps_tim10ms.LD_en = 1;
-	hps_tim10ms.INT_en = 1;
-	hps_tim10ms.enable = 1;
-
-	HPS_TIM_config_ASM(&hps_tim10ms);
-
-	while (1) {
-
-		if (pushbtn_int_flag == 1) {
-
-            start = 1;
-
-            pushbtn_int_flag = 0;
-
-        } else if (pushbtn_int_flag == 2) {
-
-            start = 0;
-
-            pushbtn_int_flag = 0;
-
-        } else if (pushbtn_int_flag == 4) {
-
-			start = 0;
-			count = 0;
-
-            pushbtn_int_flag = 0;
-
-			HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
-
-		}
-
-		if (start && hps_tim0_int_flag) {
-
-			hps_tim0_int_flag = 0;
-
-			count = (count + 1) % 999999;
-
-			HEX_write_ASM(HEX0, count % 10);
-			HEX_write_ASM(HEX1, (count / 10) % 10);
-			HEX_write_ASM(HEX2, (count / 100) % 10);
-			HEX_write_ASM(HEX3, ((count / 100) % 60) / 10);
-			HEX_write_ASM(HEX4, ((count / 100) / 60) % 10);
-			HEX_write_ASM(HEX5, ((count / 100) / 60) / 10 % 10);
-
-		}
-
-	}
-	*/
 	return 0;
 }
 
