e23bba1ab3cc4ea5ea91f985661795cf1d213ca4
Pengnan Fan <pengnan.fan@mail.mcgill.ca>
2019-11-10 13:46:47 -0500

    write a bit about clear

--- Lab4/drivers/asm/VGA.s
+++ Lab4/drivers/asm/VGA.s
@@ -1,34 +1,87 @@
 		.text
 		.equ	PIX_BUFF,	0xC8000000
+		.equ	CHR_BUFF,	0xC9000000
 		.global	VGA_clear_charbuff_ASM
 		.global	VGA_clear_pixelbuff_ASM
 		.global	VGA_write_char_ASM
 		.global	VGA_write_byte_ASM
 		.global	VGA_draw_point_ASM
 
+VGA_write_char_ASM:
+		CMP		R0,		#1
+		MOVEQ	R4,		#12
+		B	DUMMY
+
 VGA_clear_charbuff_ASM:
+		PUSH	{R0-R3,	LR}
+		MOV		R0,		#59
+		MOV		R1,		#79
+		BL		CB_CL_LP
+		POP		{R0-R3,	LR}
+		BX		LR
+
+CB_CL_LP:
+		PUSH	{LR}
+		B		CB_CL_INNER_LP
+		POP		{LR}
+		SUB		R0,		R0,		#1
+		CMP		R0,		#0
+		BXLT	LR
+		MOV		R1,		#79
+		B		CB_CL_LP
+
+CB_CL_INNER_LP:
+		CMP		R1,		#0
+		BXLT	LR
+		MOV		R2,		R0
+		LSL		R2,		#7
+		ORR		R2,		R2,		R1
+		LDR		R3,		=CHR_BUFF
+		ADD		R3,		R3,		R2
+		LDR		R2,		[R3]
+		AND		R2,		R2,		#0
+		STR		R2,		[R3]
+		SUB		R1,		R1,		#1
+		B		CB_CL_INNER_LP
 
 VGA_clear_pixelbuff_ASM:
 		PUSH	{R0-R3,	LR}
-		LDR		R0,		#239
-		LDR		R1,		#319
+		MOV		R0,		#239
+		MOV		R1,		#1
+		LSL		R1,		#8
+		ORR		R1,		R1,		#63
 		BL		PB_CL_LP
-		POP		{LR, R3, R2, R1, R0}
+		POP		{R0-R3,	LR}
 		BX		LR
+
 PB_CL_LP:
+		PUSH	{LR}
+		B		PB_CL_INNER_LP
+		POP		{LR}
+		SUB		R0,		R0,		#1
 		CMP		R0,		#0
 		BXLT	LR
+		MOV		R1,		#1
+		LSL		R1,		#8
+		ORR		R1,		R1,		#63
+		B		PB_CL_LP
+
+PB_CL_INNER_LP:
 		CMP		R1,		#0
-		MOVLT	R1,		#319
-		SUBLT	R0,		R0,		#1
+		BXLT	LR
 		MOV		R2,		R0
-		LSL		R2,		#10
-		AND		R2,		R1
-		LSL		R2,		#1
+		LSL		R2,		#9
+		ORR		R2,		R2,		R1
+		LSL		R2,		#2
 		LDR		R3,		=PIX_BUFF
 		ADD		R3,		R3,		R2
 		LDR		R2,		[R3]
 		AND		R2,		R2,		#0
 		STR		R2,		[R3]
 		SUB		R1,		R1,		#1
-		B		PB_CL_LP
+		B		PB_CL_INNER_LP
+
+DUMMY:
+		B	DUMMY
+
+		.end
--- Lab4/main.c
+++ Lab4/main.c
@@ -2,5 +2,6 @@
 #include	"./drivers/inc/VGA.h"
 
 int	main(){
+	VGA_write_char_ASM(1, 2, 'a');
 	return	0;
 }
